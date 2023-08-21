bc3bad8e959bb7f40b1abb41ca8f0be0e7f60f7c
Hzuletam <142773639+Hzuletam@users.noreply.github.com>
2023-08-21 18:36:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class Auto:
     return len(self.asientos)
     
   def verificarIntegridad(self):
-    if self.motor.registro == self.registro and all(self.registro == asiento.registro for asiento in self.asientos]:
+    if self.motor.registro == self.registro and all(self.registro == asiento.registro for asiento in self.asientos):
       return print('Auto original')
     else:
       return print('Las piezas no son originales')
