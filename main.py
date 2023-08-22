909a8697acf4dbaf3342829d95a90a46a0d8d8c1
Hzuletam <142773639+Hzuletam@users.noreply.github.com>
2023-08-21 19:51:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -43,8 +43,9 @@ class Auto:
     return len([asiento for asiento in self.asientos if isinstance(asiento,Asiento)])
     
   def verificarIntegridad(self):
-    for asiento in self.asientos:
-      if (isinstance(asiento,Asiento) and  (self.registro!= asiento.registro)) :
+    asientosValidos=[asiento for asiento in self.asientos if isinstance(asiento,Asiento)
+     for asiento in asientosValidos: 
+      if self.registro != asiento.registro :
         return 'Las piezas no son originales'
         
     if self.registro != self.motor.registro:
